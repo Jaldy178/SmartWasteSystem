caf1c0f31069e9519ab8a9b74d9847586f2f2c31
Matt4267 <matthew.mungai@strathmore.edu>
2025-07-07 10:07:33 +0300

    Adding css path

--- auth/register.php
+++ auth/register.php
@@ -62,6 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <title>Register - Smart Waste System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body class="bg-light">
     <div class="container mt-5">
